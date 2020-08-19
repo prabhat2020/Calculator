63fbe2095091369dd34561af3d5c85ab717a3b23
prabhat2020 <65116422+prabhat2020@users.noreply.github.com>
2020-08-19 15:17:45 +0530

    1st commit

---
+++ calculator.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/", function(req, res) {
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+
+  var result = num2 / (num1 * num1);
+  // console.log(req.body);
+  // res.send("Thanks for posting that");
+  res.send("Your BMI is  " + result);
+});
+
+
+app.listen(3000, function() {
+  console.log("Server started on  port 3000");
+})
